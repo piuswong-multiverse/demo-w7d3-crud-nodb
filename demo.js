ccaf50ab8c1da53e3ef9d6c855edab971f10762f
piuswong-multiverse <111539998+piuswong-multiverse@users.noreply.github.com>
2022-12-09 11:16:00 -0600

    Create POST endpoint for new car

--- demo.js
+++ demo.js
@@ -19,6 +19,37 @@ app.delete('/', (req, res) => {
     res.send("Got problems? DELETE them")
 })
 
+// CRUD endpoints with route parameters
+// POST
+
+// Dummy data to operate on
+let cars = [{
+    brand: "Tesla",
+    color: "Red"
+},
+{
+    brand: "Toyota",
+    color: "Gray"
+}, 
+{
+    brand: "Honda",
+    color: "Blue"
+}
+]
+
+// NEED MIDDLEWARE to understand the request body (payload)
+app.use(express.json()); // lets me get JSON data from the request
+app.use(express.urlencoded()); // lets me get urlencoded data from the request
+
+app.post('/cars', (req, res) => {
+    // get a new car to add...
+    const newCar = req.body;
+    cars.push(newCar);
+    // send out the whole list of cars
+    res.send(cars);
+})
+
+
 
 app.listen(port, () => {
     console.log(`We're live on http://localhost:${port}`)
