234188cfe465690ddcb9abe107a2e1b408fe1d69
piuswong-multiverse <111539998+piuswong-multiverse@users.noreply.github.com>
2022-12-08 18:37:23 -0600

    Include dummy data for cars

---
+++ dummyData.js
@@ -0,0 +1,16 @@
+// Dummy data to operate on
+let cars = [{
+    brand: "Tesla",
+    color: "Red"
+},
+{
+    brand: "Toyota",
+    color: "Gray"
+}, 
+{
+    brand: "Honda",
+    color: "Blue"
+}
+]
+
+module.exports = cars;
